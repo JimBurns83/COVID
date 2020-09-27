a78a86d7d1557af8bf133adf5d112b487f6d67ca
jimburns83 <an@example.com>
2020-09-27 14:45:00 +0000

    add datatable

--- R/ONS/Download.R
+++ R/ONS/Download.R
@@ -1,11 +1,12 @@
 library(readr)
 library(lubridate)
 library("rjson")
+library(data.table)
 if(!file.exists("ons.csv")){
   download.file("https://opendata.ecdc.europa.eu/covid19/casedistribution/csv/", "ons.csv")
 }
 
 price_list <- read_csv("ons.csv") # Point to local copy
 result <- fromJSON(file ="ons.json")
-titles <- names(as.data.frame(result$records[1]))
+titles <- names(as.data.frame(head(result$records)))
 
