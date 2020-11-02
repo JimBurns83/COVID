b96ee1b2a6f8bcc9ed405e1f069f7b416c1d396c
JimBurnsNice <55092215+JimBurnsNice@users.noreply.github.com>
2020-11-02 21:41:28 +0000

    Update Download.R

--- R/ONS/Download.R
+++ R/ONS/Download.R
@@ -18,12 +18,22 @@ as_datetime(my_time)
 date_str <- "26/09/2020"
 dd <- as_date(date_str, format="%d/%m/%Y")
 as.POSIXct.Date(dd)
-origin <- Sys.Date()
+#origin <- Sys.Date()
+origin <- as_date("01/01/2020", format="%d/%m/%Y")
 dif <- difftime(dd,origin, units = "days")
 dif <- as.integer(dif)
 
-
+dplot <- c()
 obs <- price_list["dateRep"]
 for(i in 1:nrow(obs)) {
-  print(obs[i,,1])
+  oval <- as.integer(difftime(as_date(obs[i,,1],format="%d/%m/%Y"), origin, unit= "days"))
+  dplot[i] <- oval
+  print(oval)
 }
+dat <- data.table(
+  date = dplot,
+  cases = price_list["cases"],
+  deaths = price_list["deaths"],
+  population = price_list["popData2019"],
+  country = price_list["countriesAndTerritories"]
+)
